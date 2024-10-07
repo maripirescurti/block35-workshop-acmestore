15de8f9fcb6b776fee9651839628d1c000cb956a
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-07 02:31:25 -0400

    added delet3e method

--- server/index.js
+++ server/index.js
@@ -18,6 +18,56 @@ app.use(express.json());
 
 
 //APP ROUTES
+// get - read
+app.get('/api/users', async(req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch(ex) {
+    next(ex)
+  }
+});
+
+app.get('/api/products', async(req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch(ex) {
+    next(ex)
+  }
+});
+
+app.get('/api/users/:id/favorites', async(req, res, next) => {
+  try {
+    res.send(await fetchFavorites(req.params.id));
+  } catch(ex) {
+    next(ex)
+  }
+});
+
+// post - create
+app.post('/api/users/:id/favorites', async(req, res, next) => {
+  try {
+    res.status(201).send(await createFavorite({
+      user_id: req.params.id,
+      product_id: req.body.product_id
+    }))
+  } catch(ex) {
+    next(ex);
+  }
+});
+
+// delete
+app.delete('/api/users/:userId/favorites/:id', async(req, res, next) => {
+  try {
+    await destroyFavorite({
+      user_id:req.params.userId,
+      id: req.params.id
+    });
+    res.sendStatus(204);
+  } catch(ex) {
+    next(ex);
+  }
+});
+
 
 //init 
 const init = async() => {

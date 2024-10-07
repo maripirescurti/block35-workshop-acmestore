9775e0987671d7d79add98cc04e056a169803ee6
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-07 02:01:51 -0400

    added tables

--- server/db.js
+++ server/db.js
@@ -5,6 +5,29 @@ const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 
 // methods
+const createTables = async() => {
+  const SQL = `
+    DROP TABLE IF EXISTS favorites;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+    CREATE TABLE users(
+      id UUID PRIMARY KEY,
+      username VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(255)
+    );
+    CREATE TABLE favorites(
+      id UUID PRIMARY KEY,
+      name VARCHAR(100) UNIQUE NOT NULL
+    );
+    CREATE TABLE favorites(
+      id UUID PRIMARY KEY,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      product_id UUID REFERENCES products(id) NOT NULL,
+      CONSTRAINT unique_user_id_product_id UNIQUE (user_id, product_id)
+    );
+  `;
+  await client.query(SQL);
+};
 
 // exports
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -23,6 +23,21 @@ app.use(express.json());
 const init = async() => {
   await client.connect();
   console.log('connected to database');
+  await createTables();
+  console.log('tables created');
+  const [mari, ozan, simba, mug, ball, pen, sponge] = await Promise.all([
+    createUser({ username: 'mari', password: 'g5tfrs'}),
+    createUser({ username: 'ozan', password: 'g5tfrs!!'}),
+    createUser({ username: 'simba', password: 'shhhh'}),
+    createProduct({ name: 'mug' }),
+    createProduct({ name: 'ball' }),
+    createProduct({ name: 'pen' }),
+    createProduct({ name: 'sponge' })
+  ])
+
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on ${port}`));
 };
 
 init();
